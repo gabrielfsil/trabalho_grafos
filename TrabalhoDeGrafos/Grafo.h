fa9723675e8b0ec3b921a7f2a215e6c94fcadcaa
Henrique <henriqueacsdev@gmail.com>
2020-10-26 18:38:10 -0300

    Arrumando

--- TrabalhoDeGrafos/Grafo.h
+++ TrabalhoDeGrafos/Grafo.h
@@ -14,6 +14,7 @@ class Grafo
         void imprimirMatrizPeso();
         void arvoreGeradoraPrim();
         void arvoreGeradoraMinimaKruskal();
+        void numArestas();
 
         void frequenciaRelativaGraus();
         int grauMedioDoGrafo();
